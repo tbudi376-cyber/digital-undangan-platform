9eee0b534ed9d1dcd22f869fe1a6789181415119
Tubagus Budi <tbudi376@gmail.com>
2026-05-24 20:52:57 +0700

    fix: next config and countdown nan

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,16 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
+      },
     ],
   },
 };
